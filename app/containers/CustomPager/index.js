fe32c4844c5e296554c04ce327a09bce64ca709a
Chris D'Agostino <cdagostino@thenavisway.com>
2017-09-14 14:04:50 -0700

    no message

--- app/containers/CustomPager/index.js
+++ app/containers/CustomPager/index.js
@@ -4,17 +4,31 @@ import CustomPager from './CustomPager';
 import Simple from './Simple';
 import {bindActionCreators} from 'redux';
 import {browserHistory} from 'react-router';
-import store from '../../app'
+import store from '../../app';
+import * as courseActions from '../App/actions';
 
+const stompClient = require('../../utils/websocket-listener');
 
 class ExampleGridContainer extends React.PureComponent {
     constructor(props, context) {
         super(props, context);
 
-
+        this.refreshCourses = this.refreshCourses.bind(this);
     }
 
+    refreshCourses(message) {
+        console.log(message)
+        this.props.actions.loadCourses();
+    }
 
+    componentDidMount() {
+
+        stompClient.register([
+            {route: '/topic/newCourse', callback: this.refreshCourses},
+            {route: '/topic/updateCourse', callback: this.refreshCourses},
+            {route: '/topic/deleteCourse', callback: this.refreshCourses}
+        ]);
+    }
 
     render() {
 
@@ -22,8 +36,8 @@ class ExampleGridContainer extends React.PureComponent {
         return (
             <div className="simpleContainer">
                 <h2 className="gridH2">Courses</h2>
-                
-                <CustomPager { ...{ store } } />
+
+                <CustomPager { ...{store} } />
 
             </div>
         );
@@ -36,11 +50,11 @@ const mapStateToProps = (state) => ({
     store: state
 });
 
-const mapDispatchToProps = (dispatch) => {
+function mapDispatchToProps(dispatch) {
     return {
-        dispatch
-    }
-};
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
 
 
 //connect component to redux, connect returns a function that takes CoursesPage as a parameter
---
+++ app/utils/websocket-listener.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ChrisDAgostino on 9/14/17.
+ */
+'use strict';
+
+var SockJS = require('sockjs-client'); // <1>
+require('stompjs'); // <2>
+
+function register(registrations) {
+    var socket = SockJS('http://localhost:8080/coursesSocket'); // <3>
+    var stompClient = Stomp.over(socket);
+    stompClient.connect({}, function(frame) {
+        registrations.forEach(function (registration) { // <4>
+            stompClient.subscribe(registration.route, registration.callback);
+        });
+    });
+}
+
+module.exports.register = register;
